ccd9f4ff2cf4bf935ac711243e002d85fc13d74c
kittycat959 <37156676+kittycat959@users.noreply.github.com>
2021-07-12 20:17:48 +0100

    Update program.py
    made the code work

--- program.py
+++ program.py
@@ -12,26 +12,26 @@ pin_to_use_filter = 40 #the gpio pin number to output the pwm signal on for the 
 pin_to_use_resonance = 38 #the gpio pin number to output the pwm signal on for the resonance control coltage
 filter_percentage = 100 #the duty cycle of the pin connected to the filter control voltage
 resonance_percentage = 0 #the duty cycle of the pin connected to the resonance control voltage
-time_for_filter_change = 10 #the amount of time in seconds between filter changes
+time_for_filter_change = 1 #the amount of time in seconds between filter changes
 video_url = "" #the url of the video to check the chat on (put it between the apostaphies)
 
 def pwm(time_for_filter_change): #defines the pwm function with the argument time_for_filter_change
     #this section sets up the first gpio pin for the filter cv
-    GPIO.setmode(GPIO.BCM) 
+    GPIO.setmode(GPIO.BOARD) 
     GPIO.setup(pin_to_use_filter, GPIO.OUT)
     filter_pwm = GPIO.PWM(pin_to_use_filter, 8000)
     filter_pwm.start(filter_percentage)
 
     #this section sets up the second gpio pin for the resonance cv
-    GPIO.setmode(GPIO.BCM)
     GPIO.setup(pin_to_use_resonance, GPIO.OUT)
     resonance_pwm = GPIO.PWM(pin_to_use_resonance, 8000)
     resonance_pwm.start(resonance_percentage)
 
     #updates the dutycycle of the two pwm pins with the delay defines in time_for_filter_change
     while True:
-        filter_pwm.ChangeDutyCycle(filter_percentage)
-        resonance_pwm.ChangeDutyCycle(resonance_percentage)
+        print(filter_percentage)
+        #filter_pwm.ChangeDutyCycle(filter_percentage)
+        #resonance_pwm.ChangeDutyCycle(resonance_percentage)
         time.sleep(time_for_filter_change)
 
 #creates and starts a thread for the pwm function to ensure there are no drop outs when we read the messages
@@ -39,11 +39,16 @@ pwm_thread = threading.Thread(target=pwm, args=([time_for_filter_change]))
 pwm_thread.start()
 
 def message_retriever(video_url): #defines the message_retriever function with the argument video_url
+    #ensures we are using global and not local namespace for these variables
+    global filter_percentage 
+    global resonance_percentage
+
     #this section gets the new messages from the chat
     chat = pytchat.create(video_url)
     while chat.is_alive():
         for c in chat.get().sync_items():
             message = f"{c.message}"
+            print(message)
 
             #this section preforms a series of checks to validate the input from the chat for the filter percentage
             if len(message) > 7 and len(message) < 11:
@@ -58,5 +63,5 @@ def message_retriever(video_url): #defines the message_retriever function with t
                     if message[11:len(message)].isdigit():
                         if int(message[11:len(message)]) < 101:
                             resonance_percentage = int(message[11:len(message)])
-
+            
 message_retriever(video_url) #calls the message_retriever function with the argument video_url
